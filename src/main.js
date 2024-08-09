c82203687ced8f1fc12cb1daa05051af7d47bf9c
Mp2212 <patelmitul38@gmail.com>
2024-08-09 11:10:06 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+
+
+
+
+
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+import './assets/css/style.css';
+// Import Bootstrap JS (optional, if you need Bootstrap's JavaScript components)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+
+
+createApp(App).mount('#app')
